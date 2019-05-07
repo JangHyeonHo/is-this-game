b30a9fe96dd9bc0b24ecc45b99a0b16fc2ab7fc0
gusghwkd202 <gusghwkd202@naver.com>
2019-05-07 18:09:46 +0900

    commit

---
+++ src/com/miridih/exam/RotatedRect.java
@@ -0,0 +1,22 @@
+package com.miridih.exam;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 회전 사각형 프로그램 스켈레톤
+ * */
+public class RotatedRect implements Solver {
+	
+	public static void main(String[] args) {
+		//Stock이라 되어 있는 부분을 수정했습니다.
+        new RotatedRect().solve(System.in, System.out);        
+    }
+
+	@Override
+	public void solve(InputStream in, PrintStream out) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/miridih/exam/Stock.java
@@ -0,0 +1,27 @@
+package com.miridih.exam;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 주식 투자 프로그램의 스켈레톤
+ * */
+public class Stock implements Solver {
+	
+	public static void main(String[] args) {
+        new Stock().solve(System.in, System.out);        
+    }
+
+    /**
+     * 문제 풀이 메인 메소드   
+     * @param in 테스트 케이스 입력을 받이들이는 InputStream
+     * @param out 결과값을 출력할 PrintStream 
+     */
+
+	@Override
+	public void solve(InputStream in, PrintStream out) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
